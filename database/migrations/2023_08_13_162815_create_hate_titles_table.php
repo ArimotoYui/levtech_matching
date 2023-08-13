bd3cacf91ff8072f6e55d7078964a55aac64a1ae
ArimotoYui <akasatana05280708@gmail.com>
2023-08-13 18:23:57 +0900

    マイグレーション１

--- database/migrations/2023_08_13_162815_create_hate_titles_table.php
+++ database/migrations/2023_08_13_162815_create_hate_titles_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('hate_titles', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title', 50);
         });
     }
 
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('hate_titles');
     }
 };
--- database/migrations/2023_08_13_162845_create_posts_table.php
+++ database/migrations/2023_08_13_162845_create_posts_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title', 50);
+            $table->string('body', 200);
+            $table->foreignId('user_id');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
